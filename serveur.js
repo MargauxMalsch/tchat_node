c5990741166d316fad0571c2c001f4733df3ee1a
Margaux <margaux.malsch@edu.devinci.fr>
2022-01-05 13:45:16 +0100

    creation and connexion to the bdd

--- serveur.js
+++ serveur.js
@@ -2,6 +2,17 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+
+mongoose
+  .connect("mongodb+srv://margo:Password@allo.lh527.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'))
+
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
