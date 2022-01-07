d63343caca9c5014aad3ab8d8f81679f640daaf9
Nicolas Mopin <Nicolas.mopin@edu.devinci.fr>
2022-01-07 14:01:28 +0100

    :beers: LETSGOOOOOO MSN FINITO PLACE A LA MNM YIHAAA DEDICACE AU VLOG DE POPOW :boom: :boom:

--- serveur.js
+++ serveur.js
@@ -3,6 +3,8 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://margo:Password@allo.lh527.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose
   .connect("mongodb+srv://margo:Password@allo.lh527.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
@@ -15,13 +17,31 @@ mongoose
 
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db('myFirstDatabase');
+        dbo.collection('msgs').find({}).toArray(function (err, result) {
+            if (err) throw err;
+            res.sendFile(__dirname + '/index.html', result);
+            db.close();
+        });
+    });
+
+    // res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/quoi', (req, res) => {
-    res.sendFile(__dirname + '/bdd.js');
+app.get('/msg', (req, res) => {
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db('myFirstDatabase');
+        dbo.collection('msgs').find({}).toArray(function (err, result) {
+            if (err) throw err;
+            // console.log(result);
+            res.send(result);
+            db.close();
+        });
+    });
 });
-
 
 var msgSchema = new mongoose.Schema({
     pseudo: String,
@@ -31,19 +51,22 @@ var msgSchema = new mongoose.Schema({
 
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-    console.log(msg)
+      console.log(msg)
 
-      var msgModel = mongoose.model('msg', msgSchema);
 
-      var monMsg = new msgModel({pseudo : 'Nvcq'});
-      monMsg.message = msg;
+          io.emit('chat message', msg);
+         var msgModel = mongoose.model('msg', msgSchema);
 
-      monMsg.save(function (err) {
-          if (err) { throw err; }
-          console.log('Commentaire ajouté avec succès !');
-          mongoose.connection.close();
-      });
+          var monMsg = new msgModel({pseudo : 'Nvcq'});
+          monMsg.message = msg;
+
+          monMsg.save(function (err) {
+              if (err) { throw err; }
+              console.log('Commentaire ajouté avec succès !');
+              //mongoose.connection.close();
+          });
+
+
   });
 });
 
